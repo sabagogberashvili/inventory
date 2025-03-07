624b3dc9e0f67ba30f6d7f7f6a74035e0d6e1fd8
saba gogberashvili <sgogberashvili@gmail.com>
2025-03-07 14:49:30 +0400

    first commit

--- items/urls.py
+++ items/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from products.views import ItemViewSet, CategoryViewSet
+from items.views import ItemViewSet, CategoryViewSet
 
 router = DefaultRouter(use_regex_path=False)
 router.register('items', ItemViewSet)
